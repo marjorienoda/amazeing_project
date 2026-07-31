d2f1298932a7177667d672a574bdf48fd80697cc
Runa Kato <rkato@c6r4s2.42tokyo.jp>
2026-07-31 17:01:18 +0900

    test

--- display.py
+++ display.py
@@ -1,11 +1,30 @@
 class MazeGenerator:
-	def __init__(self, grid:list[list[Cell]], entry:tuple[int, int], exit:tuple[int, int]):
-		self.grid = grid
-		self.entry = entry
-		self.exit = exit
+	def __init__(self, width, height, seed, entry, exit):
+		self.width = width
+		self.height = height
+		self.seed = seed
+		self.grid: list[list[Cell]] = []
+		self.entry: tuple[int, int] = entry
+		self.exit: tuple[int, int] = exit
+	def generator():
+		
+		return grid
+
+	def solve():
+		self.entry
+		self.exit
+		if hashdhhd
+		else:
+				hdifidjfis
 
 
-def display(maze: MazeGenerator):
+class Cell:
+	def __init__(self, x, y, north, east, south, west)
+		self.x = x
+		self.y = y
+		self.
+
+def make_grid(maze: MazeGenerator):
 	rows = len(maze.grid)
 	cols = len(maze.grid[0])
 	new_grid = [[" " for _ in range(2 * cols + 1)] for _ in range(2 * rows + 1)]
@@ -13,7 +32,7 @@ def display(maze: MazeGenerator):
 
 
 def main() -> None:
-	test = MazeGenerator(grid=[[CellClass], [9, 5, 6], [12, 5, 7]], entry=(0, 0), exit=(2, 2))
+	test = MazeGenerator(grid=[[Cell], [9, 5, 6], [12, 5, 7]], entry=(0, 0), exit=(2, 2))
 	print(test.grid)
 	print(test.entry)
 	print(test.exit)
